8b0d69061ecbe17f28f58f92074371cec598a037
Terlihat <142712639+Terlihat@users.noreply.github.com>
2026-06-18 04:35:57 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'kasir-pintar-v2.2.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.ico',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install Service Worker dan simpan file ke Cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Gunakan file dari Cache saat Offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response; // Kembalikan dari cache
+      }
+      return fetch(event.request); // Ambil dari internet jika tidak ada di cache
+    })
+  );
+});
